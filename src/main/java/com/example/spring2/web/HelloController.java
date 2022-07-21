f622e482a7326fb2a7af3c380779a4e687da49e3
aischool <git@github.com:mygod76/GJAI_Study01.git>
2022-07-21 17:52:21 +0900

    2022_07_21 DTO 테스트 완료 77Page

--- src/main/java/com/example/spring2/web/HelloController.java
+++ src/main/java/com/example/spring2/web/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.spring2.web;
 
+import com.example.spring2.web.dto.HelloResponsDto;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -8,6 +10,12 @@ public class HelloController {
 
     @GetMapping("/hello")   // getmapping
     public String hello() {
+
         return "hello";
     }
+    @GetMapping("/hello/dto")
+    public HelloResponsDto helloDto(@RequestParam("name") String name ,@RequestParam("amount") int amount){
+        return new HelloResponsDto(name, amount);
+    }
+
 }
