f169c4b507d232fb2478fa20a8844f45c75edfbd
aischool <git@github.com:mygod76/GJAI_Study01.git>
2022-07-22 17:45:21 +0900

    Spring Data JPA 테스트 코드 작성완료

---
+++ src/main/java/com/example/spring2/domain/posts/Posts.java
@@ -0,0 +1,33 @@
+package com.example.spring2.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+@Getter // 6
+@NoArgsConstructor // 5
+@Entity // 1
+public class Posts {
+
+    @Id // 2
+    @GeneratedValue(strategy= GenerationType.IDENTITY) // 3
+    private long  id; // 4
+
+    @Column(length=500,nullable=false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT",nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder // 7
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+
+    }
+
+}
